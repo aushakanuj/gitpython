d590c44032f754486252d7f5e258051250222aab
aushak <aushakanuj@gmail.com>
2020-04-04 08:56:16 +0530

    update

--- python/codes/test.py
+++ python/codes/test.py
@@ -1,18 +1,7 @@
-placement = "Beaches are cool places to visit in spring however the Mackinaw Bridge is near. Most people visit Mackinaw later since the island is a cool place to explore."
-f=placement.strip().split()
-print(f)
-d={}
-
-for i in placement:
-    if i not in d:
-        d[i]=0
-    d[i]=d[i]+1
-
-key=list(d.keys())
-min=key[0]
-for i in key:
-    if d[i]<d[min]:
-        min=i
-min_value=i
-print(d)
-print(min)
+i=0
+e=[]
+while i <=15:
+    if i %2==0:
+        e.append(i)
+    i=i+1
+print(e)
