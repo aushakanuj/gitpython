130013cc125c1af7dbea7648f0f8678dbecf8749
aushak <aushakanuj@gmail.com>
2020-04-06 23:24:44 +0530

    april 6th

--- codes/week4:1.py
+++ codes/week4:1.py
@@ -1,6 +1,5 @@
 initial = 7
-def f(x, y =3, z=initial):
-	print("x, y, z, are: " + str(x) + ", " + str(y) + ", " + str(z))
-	
-initial = 10
-f(2)
+def f(x, y = 3, z = initial):
+    print("x, y, z are:", x, y, z)
+
+f(2, z = 10)
