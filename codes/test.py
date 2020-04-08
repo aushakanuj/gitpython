2ddea94cf2c2cf455747920880f37241d065567e
aushak <aushakanuj@gmail.com>
2020-04-08 17:56:50 +0530

    new project added

--- codes/test.py
+++ codes/test.py
@@ -1,3 +1,10 @@
-import random
-for i in range(0,3):
-    print(random.randrange(0,45))
+L = ['E', 'F', 'B', 'A', 'D', 'I', 'I', 'C', 'B', 'A', 'D', 'D', 'E', 'D']
+
+d = {}
+for x in L:
+    if x in d:
+        d[x] = d[x] + 1
+    else:
+        d[x] = 1
+for x in sorted(d,key=lambda x: d[x],reverse=True):
+    print("{} appears {} times".format(x, d[x]))
---
+++ codes/test1.py
@@ -0,0 +1,4 @@
+
+dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+d=dictionary
+sorted_values=sorted(d,reverse=True,key=lambda x:d[x])
