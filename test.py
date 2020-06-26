7840a3f4a49a9974263349e304be0b6915a5b52b
aushak <aushakanuj@gmail.com>
2020-06-26 19:51:19 +0530

    hello

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-with open("final.txt","w") as d:
-
-    with open("hello.txt","r") as f:
-        
-        g=f.readlines()
-        s=""
-        for num in range(len(g)):
-            if num%2==0:
-                r=g[num].strip()
-                s+=" "+r     
-
-        d.write(s)
+a,b=input().split()
