50b0c73caaa4fd1071bc47df73496cd6fb661d77
aushak <aushakanuj@gmail.com>
2020-07-06 00:31:53 +0530

    DSA

---
+++ project/.ipynb_checkpoints/code-checkpoint.py
@@ -0,0 +1,51 @@
+def getPositiveWords():
+    positiveWords=[]
+    with open("positive_words.txt",'r') as p:
+        lines=p.readlines()
+        for word in lines:
+           if word[0]!=";" and word[0]!="\n":
+                positiveWords.append(word.strip())
+    p.close()
+    return positiveWords
+
+def getNegativeWords():
+    negativeWords=[]
+    with open("negative_words.txt",'r') as p:
+        lines=p.readlines()
+        for word in lines:    
+            if word[0]!=";" and word[0]!="\n":
+                negativeWords.append(word[0].strip())
+    p.close()
+    return negativeWords
+
+with open("test.csv","w") as w:
+    with open("project_twitter_data.csv","r") as f:
+             
+        lines=f.readlines()
+        positiveWords=getPositiveWords()
+        negativeWords=getNegativeWords()
+        header=lines[0].strip().split(",")
+        w.write("{},{},{},{}\n".format(header[1],header[2],"Positive count","Negative count"))
+        for row in lines[1:]:
+            pw=0
+            nw=0
+            val=row.strip().split(',')
+            punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
+            a=""
+            a=val[0]
+            for i in punctuation_chars:
+                a=(a.replace(i,""))
+            a=a.lower()
+            a=a.split(" ")
+            for i in a:
+                if i in positiveWords:
+                    pw=pw+1
+                if i in negativeWords:
+                    nw=nw+1
+            w.write("{},{},{},{} \n".format(val[1],val[2],pw,nw))
+    f.close()
+w.close()
+
+
+
+
--- project2/movie.py
+++ project2/movie.py
@@ -1,77 +1,86 @@
-import requests
 import json
 
+import requests
+
+
 def get_movies_from_tastedive(a):
-    baseUrl="https://tastedive.com/api/similar"
-    key="364383-movies-XCYHUSC3"
-    mov=a
-    param={}
-    param["q"]=mov
-    param["k"]=key
-    param["limit"]=5
-    param["type"]="movies"
-    resp=requests.get(baseUrl,params=param)
+    baseUrl = "https://tastedive.com/api/similar"
+    key = "364383-movies-XCYHUSC3"
+    mov = a
+    param = {}
+    param["q"] = mov
+    param["k"] = key
+    param["limit"] = 5
+    param["type"] = "movies"
+    resp = requests.get(baseUrl, params=param)
     print(resp.url)
-    p=resp.json()
+    p = resp.json()
     return p
 
 
 def extract_movie_titles(a):
-    mov=[i["Name"] for i in a["Similar"]["Results"]]
+    mov = [i["Name"] for i in a["Similar"]["Results"]]
     return mov
 
 
 def get_related_titles(list_movies):
-    listOfRealtedMovies=[]
+    listOfRealtedMovies = []
     for i in list_movies:
-        fiveMovies=get_movies_from_tastedive(i)
-        listOfFiveMovies=extract_movie_titles(fiveMovies)
+        fiveMovies = get_movies_from_tastedive(i)
+        listOfFiveMovies = extract_movie_titles(fiveMovies)
         for i in listOfFiveMovies:
             if i not in listOfRealtedMovies:
                 listOfRealtedMovies.append(i)
     return listOfRealtedMovies
 
+
 def get_movie_rating(movs):
-    api="e2a46df1"
-    baseUrl="http://www.omdbapi.com/?apikey=35c7267d&"
-    rat=[]
-    ratings={}
+    api = "e2a46df1"
+    baseUrl = "http://www.omdbapi.com/?apikey=35c7267d&"
+    rat = []
+    ratings = {}
     for i in movs:
-        para={}
-        para["t"]=i
-        para["r"]="json"
-        resp=requests.get(baseUrl,params=para)
-        p=resp.json()
+        para = {}
+        para["t"] = i
+        para["r"] = "json"
+        resp = requests.get(baseUrl, params=para)
+        p = resp.json()
         rat.append(p)
 
     for i in rat:
-        if i["Response"]=="True":
+        if i["Response"] == "True":
             if "imdbRating" in i.keys():
-                ratings[i["Title"]]=(i["imdbRating"])
-            else:    
-                ratings[i["Title"]]="0.0"
-    return ratings   
+                ratings[i["Title"]] = i["imdbRating"]
+            else:
+                ratings[i["Title"]] = "0.0"
+    return ratings
+
 
 def getSortedMovies(a):
-    sorttedList=sorted(a.keys(),key=lambda x: a[x],reverse=True)
-    temp=[]
+    sorttedList = sorted(a.keys(), key=lambda x: a[x], reverse=True)
+    temp = []
     for i in sorttedList:
-        if a[i]=="100%":
+        if a[i] == "100%":
             temp.append(i)
             sorttedList.remove(i)
-    
+
     temp.sort()
-    sorttedList=temp+sorttedList
+    sorttedList = temp + sorttedList
     for i in sorttedList:
-        print(i," : ",a[i])
-a=1
-while a!=0:
-    movieName=input("Enter the movie name for recommendation:\n")
-    li=get_movies_from_tastedive(movieName)
-    titles=extract_movie_titles(li)
+        print(i, " : ", a[i])
 
-    combinedlist=get_related_titles(titles)
-    movieRatings=get_movie_rating(combinedlist)
+
+a = 1
+while a != 0:
+    movieName = input("Enter the movie name for recommendation:\n")
+    li = get_movies_from_tastedive(movieName)
+    titles = extract_movie_titles(li)
+
+    combinedlist = get_related_titles(titles)
+    movieRatings = get_movie_rating(combinedlist)
     getSortedMovies(movieRatings)
-    a=int(input("Enter (0) to exit the program or press (1) to ask another recommendation\n"))
-
+    a = int(
+        input(
+            "Enter (0) to exit the program or press (1) to ask another recommendation\n"
+        )
+    )
