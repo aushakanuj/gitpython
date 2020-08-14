5728d43c97d62dc3ac7a53ddd123ef27fb81ec9c
aushak anuz <aushakanuz@aushaks-MacBook-Air.local>
2020-08-15 00:22:29 +0530

    nothing

--- hack/trial.py
+++ hack/trial.py
@@ -1,6 +1,8 @@
 import gspread
 from oauth2client import client
 from oauth2client.service_account import ServiceAccountCredentials
+import pyperclip
+
 
 scope = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -8,13 +10,37 @@ scope = [
     "https://www.googleapis.com/auth/drive",
 ]
 
-path = "/Users/aushakanuz/Downloads/sheetconnection-286007-2ef7debc8d9c.json"
+path = "/Users/aushakanuz/Downloads/aushakanuj@gmail.com Firefox Recovery Codes.txt"
 creds = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
 
 client = gspread.authorize(creds)
 
 sheet = client.open("trial").sheet1
+sheet2 = client.open("trial").get_worksheet(2)
 
-# print(sheet.get_all_records())
+print(client.open("trial").worksheets())
 
-print(sheet.row_values(3))
+recent_value = "start"
+qnum = 1
+lines = 2
+questions = []
+
+print("Program has started")
+
+while True:
+
+    temp_val = pyperclip.paste()
+
+    if temp_val != recent_value and (temp_val not in questions):
+
+        try:
+            sheet.insert_row([qnum, temp_val], lines)
+            sheet.insert_row(["", ""], lines + 1)
+            sheet.insert_row(["", ""], lines + 2)
+            questions.append(temp_val)
+            print("Pasted Question{}".format(qnum))
+            qnum += 1
+            lines += 3
+        except:
+
+            raise ValueError
