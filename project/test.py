4c7de7a55b4d166a50335bf15945a83504323387
aushak anuz <aushakanuz@aushaks-MacBook-Air.local>
2020-08-15 19:14:18 +0530

    nothing

--- project/test.py
+++ project/test.py
@@ -1,17 +1,16 @@
-with open("project_twitter_data.csv","r") as f:
-    lines=f.readlines()
+with open("project_twitter_data.csv", "r") as f:
+    lines = f.readlines()
     # #positiveWords=getPositiveWords()
     # negativeWords=getNegativeWords()
-    pw=0
-    nw=0
+    pw = 0
+    nw = 0
     for row in lines:
-        val=row.strip().split(',')
-        punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@','-',"?","…"]
-        a=""
-        a=val[0]
+        val = row.strip().split(',')
+        punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@', '-', "?", "…"]
+        a = ""
+        a = val[0]
         for i in punctuation_chars:
-            a=(a.replace(i,""))
-        a=a.lower()
-        a=a.split(" ")
+            a = (a.replace(i, ""))
+        a = a.lower()
+        a = a.split(" ")
         print(a)
-    
